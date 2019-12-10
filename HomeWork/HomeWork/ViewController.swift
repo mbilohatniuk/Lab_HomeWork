592912e84adf90f8645f486fd1a0bbc7489b9f68
mbilohatniuk <m.bilohatniuk@gmail.com>
2019-12-10 15:36:47 +0200

    initial commit

---
+++ HomeWork/HomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by Maksym Bilohatniuk on 12/10/19.
+//  Copyright © 2019 Maksym Bilohatniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
