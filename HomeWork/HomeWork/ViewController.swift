116b5145b36ea31d1dcbc169e456f0b56da30ea5
mbilohatniuk <m.bilohatniuk@gmail.com>
2019-12-13 15:08:38 +0200

    lesson8 added @IB(Antion/Autlets

--- HomeWork/HomeWork/ViewController.swift
+++ HomeWork/HomeWork/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftCardImageView: UIImageView!
+    @IBOutlet weak var rightCardImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func dealButtonTapped(_ sender: UIButton) {
+        print("Tapped")
+    }
 }
 
